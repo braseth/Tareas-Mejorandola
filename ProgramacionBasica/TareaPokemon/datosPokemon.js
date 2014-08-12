78333c1193c48a5e3363f42401b105c9650bd019
Jeremy Braseth Elizondo Castro <braseth.16@gmail.com>
2014-08-11 21:34:59 -0600

    Tarea Pokemon
    Datos Pokemo: Vida, Ataque, Grito.
    Imagen del Pokemon "Dinámica"

---
+++ ProgramacionBasica/TareaPokemon/datosPokemon.js
@@ -0,0 +1,29 @@
+function Pokemon (n,v,a)
+{
+	this.grito = "Rata!";
+	this.nombre = n;
+	this.vida = v;
+	this.ataque = a;
+	this.gritar = function()
+	{
+		alert(this.grito);
+	}
+}
+
+function inicio(x)
+{
+	//Creacion de los pokemon
+	var rattata = new Pokemon("Rattata", 40 , 2);
+	var squirtle = new Pokemon("Squirtle", 90 , 45);
+	squirtle.grito = "Squilo!";
+	var charmander = new Pokemon("Charmander", 100 , 50);
+	charmander.grito = "Char!";
+	//Vector con los pokemon 
+	var pokemon = [rattata,squirtle,charmander]
+	//Otorgar informacion a los elementos del html
+	nombrePokemon.innerText = pokemon[x].nombre;
+	imagenPokemon.src = "images/"+pokemon[x].nombre+".png";
+	datosPokemon.innerText = "Vida= " + pokemon[x].vida + "\nAtaque= " + pokemon[x].ataque +
+							 "\n Grito= "+pokemon[x].grito  
+}
+
